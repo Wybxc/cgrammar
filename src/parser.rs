d94c43256e644597dccfc9d8c1cf1b41b9d41c9b
Jiayi Zhuang <wybxc@qq.com>
2025-08-06 22:12:57 +0800

    feat: add relational and bitwise operators to expression parser

--- src/parser.rs
+++ src/parser.rs
@@ -37,6 +37,19 @@ pub fn expression<'a>() -> impl Parser<'a, &'a [BalancedToken], Expression> + Cl
                 infix(left(1000 - 30), op!(Percent), binary!(Modulo)),
                 infix(left(1000 - 40), op!(Plus), binary!(Add)),
                 infix(left(1000 - 40), op!(Minus), binary!(Subtract)),
+                infix(left(1000 - 50), op!(LeftShift), binary!(LeftShift)),
+                infix(left(1000 - 50), op!(RightShift), binary!(RightShift)),
+                infix(left(1000 - 60), op!(Less), binary!(Less)),
+                infix(left(1000 - 60), op!(LessEqual), binary!(LessEqual)),
+                infix(left(1000 - 60), op!(Greater), binary!(Greater)),
+                infix(left(1000 - 60), op!(GreaterEqual), binary!(GreaterEqual)),
+                infix(left(1000 - 70), op!(Equal), binary!(Equal)),
+                infix(left(1000 - 70), op!(NotEqual), binary!(NotEqual)),
+                infix(left(1000 - 80), op!(Ampersand), binary!(BitwiseAnd)),
+                infix(left(1000 - 90), op!(Caret), binary!(BitwiseXor)),
+                infix(left(1000 - 100), op!(Pipe), binary!(BitwiseOr)),
+                infix(left(1000 - 110), op!(LogicalAnd), binary!(LogicalAnd)),
+                infix(left(1000 - 120), op!(LogicalOr), binary!(LogicalOr)),
             ))
     })
 }
