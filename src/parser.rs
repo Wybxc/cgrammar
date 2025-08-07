32875db47edd656bf9b9ff056a88d2bcf80ae86e
Jiayi Zhuang <wybxc@qq.com>
2025-08-07 10:05:52 +0800

    feat: comma expression

--- src/parser.rs
+++ src/parser.rs
@@ -3,110 +3,39 @@ use chumsky::prelude::*;
 
 type Extra<'a> = chumsky::extra::Err<Rich<'a, BalancedToken>>;
 
+/// (6.5.17) expression
 pub fn expression<'a>() -> impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone {
-    use chumsky::pratt::*;
-
-    macro_rules! op {
-        ($punct:expr) => {{
-            use Punctuator::*;
-            select! {
-                BalancedToken::Punctuator(p) if p == $punct => ()
-            }
-        }};
-    }
-
-    macro_rules! binary {
-        ($op:expr) => {
-            |left, _, right, _| {
-                use BinaryOperator::*;
-                Expression::Binary(BinaryExpression {
-                    operator: $op,
-                    left: Box::new(left),
-                    right: Box::new(right),
-                })
-            }
-        };
-    }
-
-    // Suppose precedence is X, use 1000 - 10*X as the associativity level
     recursive(|expression| {
-        let unary = unary_expression(expression.clone());
-        let cast = cast_expression(unary.clone());
-        let postfix = postfix_expression(expression.clone());
+        let binary = binary_expression(expression.clone());
+        let conditional = conditional_expression(binary, expression.clone());
+        let assignment =
+            assignment_expression(conditional.clone(), unary_expression(expression.clone()));
 
-        choice((
-            unary.map(Expression::Unary),
-            cast.map(Expression::Cast),
-            postfix.map(Expression::Postfix),
-        ))
-        .pratt((
-            infix(left(1000 - 30), op!(Star), binary!(Multiply)),
-            infix(left(1000 - 30), op!(Slash), binary!(Divide)),
-            infix(left(1000 - 30), op!(Percent), binary!(Modulo)),
-            infix(left(1000 - 40), op!(Plus), binary!(Add)),
-            infix(left(1000 - 40), op!(Minus), binary!(Subtract)),
-            infix(left(1000 - 50), op!(LeftShift), binary!(LeftShift)),
-            infix(left(1000 - 50), op!(RightShift), binary!(RightShift)),
-            infix(left(1000 - 60), op!(Less), binary!(Less)),
-            infix(left(1000 - 60), op!(LessEqual), binary!(LessEqual)),
-            infix(left(1000 - 60), op!(Greater), binary!(Greater)),
-            infix(left(1000 - 60), op!(GreaterEqual), binary!(GreaterEqual)),
-            infix(left(1000 - 70), op!(Equal), binary!(Equal)),
-            infix(left(1000 - 70), op!(NotEqual), binary!(NotEqual)),
-            infix(left(1000 - 80), op!(Ampersand), binary!(BitwiseAnd)),
-            infix(left(1000 - 90), op!(Caret), binary!(BitwiseXor)),
-            infix(left(1000 - 100), op!(Pipe), binary!(BitwiseOr)),
-            infix(left(1000 - 110), op!(LogicalAnd), binary!(LogicalAnd)),
-            infix(left(1000 - 120), op!(LogicalOr), binary!(LogicalOr)),
-        ))
-        .foldl(
-            {
-                let question = select! {
-                    BalancedToken::Punctuator(Punctuator::Question) => ()
-                };
-                let colon = select! {
-                    BalancedToken::Punctuator(Punctuator::Colon) => ()
-                };
-                question
-                    .ignore_then(expression.clone())
-                    .then_ignore(colon)
-                    .then(expression)
+        let comma = select! {
+            BalancedToken::Punctuator(Punctuator::Comma) => ()
+        };
+        assignment
+            .clone()
+            .then(
+                comma
+                    .ignore_then(assignment)
                     .repeated()
-            },
-            |condition, (then_expr, else_expr)| {
-                Expression::Conditional(ConditionalExpression {
-                    condition: Box::new(condition),
-                    then_expr: Box::new(then_expr),
-                    else_expr: Box::new(else_expr),
-                })
-            },
-        )
-        .pratt(infix(
-            right(1000 - 140),
-            select! {
-                BalancedToken::Punctuator(Punctuator::Assign) => AssignmentOperator::Assign,
-                BalancedToken::Punctuator(Punctuator::AddAssign) => AssignmentOperator::AddAssign,
-                BalancedToken::Punctuator(Punctuator::SubAssign) => AssignmentOperator::SubAssign,
-                BalancedToken::Punctuator(Punctuator::MulAssign) => AssignmentOperator::MulAssign,
-                BalancedToken::Punctuator(Punctuator::DivAssign) => AssignmentOperator::DivAssign,
-                BalancedToken::Punctuator(Punctuator::ModAssign) => AssignmentOperator::ModAssign,
-                BalancedToken::Punctuator(Punctuator::AndAssign) => AssignmentOperator::AndAssign,
-                BalancedToken::Punctuator(Punctuator::OrAssign) => AssignmentOperator::OrAssign,
-                BalancedToken::Punctuator(Punctuator::XorAssign) => AssignmentOperator::XorAssign,
-                BalancedToken::Punctuator(Punctuator::LeftShiftAssign) => AssignmentOperator::LeftShiftAssign,
-                BalancedToken::Punctuator(Punctuator::RightShiftAssign) => AssignmentOperator::RightShiftAssign,
-            },
-            |left, operator, right, _| Expression::Assignment(AssignmentExpression {
-                operator,
-                left: Box::new(left),
-                right: Box::new(right),
-            }),
-        ))
-        .labelled("expression")
-        .as_context()
+                    .collect::<Vec<Expression>>(),
+            )
+            .map(|(first, mut rest)| {
+                if rest.is_empty() {
+                    first
+                } else {
+                    rest.insert(0, first);
+                    Expression::Comma(CommaExpression { expressions: rest })
+                }
+            })
+            .labelled("expression")
+            .as_context()
     })
 }
 
+/// (6.5.1) primary expression
 pub fn primary_expression<'a>(
     expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone,
 ) -> impl Parser<'a, &'a [BalancedToken], PrimaryExpression, Extra<'a>> + Clone {
@@ -137,6 +66,7 @@ pub fn primary_expression<'a>(
     .as_context()
 }
 
+/// (6.5.3) unary expression
 pub fn unary_expression<'a>(
     expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
 ) -> impl Parser<'a, &'a [BalancedToken], UnaryExpression, Extra<'a>> + Clone {
@@ -181,12 +111,14 @@ pub fn unary_expression<'a>(
     })
 }
 
+/// (6.5.4) cast expression
 pub fn cast_expression<'a>(
     unary_expression: impl Parser<'a, &'a [BalancedToken], UnaryExpression, Extra<'a>> + Clone + 'a,
 ) -> impl Parser<'a, &'a [BalancedToken], CastExpression, Extra<'a>> + Clone {
     recursive(|_cast_expression| unary_expression.map(CastExpression::Unary))
 }
 
+/// (6.5.2) postfix expression
 pub fn postfix_expression<'a>(
     expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
 ) -> impl Parser<'a, &'a [BalancedToken], PostfixExpression, Extra<'a>> + Clone {
@@ -227,27 +159,151 @@ pub fn postfix_expression<'a>(
         .as_context()
 }
 
-pub fn conditional_expression<'a>(
+/// (6.5.5) multiplicative expression
+///
+/// (6.5.6) additive expression
+///
+/// (6.5.7) shift expression
+///
+/// (6.5.8) relational expression
+///
+/// (6.5.9) equality expression
+///
+/// (6.5.10) AND expression
+///
+/// (6.5.11) exclusive OR expression
+///
+/// (6.5.12) inclusive OR expression
+///
+/// (6.5.13) logical AND expression
+///
+/// (6.5.14) logical OR expression
+pub fn binary_expression<'a>(
     expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
-) -> impl Parser<'a, &'a [BalancedToken], ConditionalExpression, Extra<'a>> + Clone {
-    let question = select! {
-        BalancedToken::Punctuator(Punctuator::Question) => ()
-    };
-    let colon = select! {
-        BalancedToken::Punctuator(Punctuator::Colon) => ()
-    };
+) -> impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone {
+    use chumsky::pratt::*;
 
-    expression
-        .clone()
-        .then_ignore(question)
-        .then(expression.clone())
-        .then_ignore(colon)
-        .then(expression)
-        .map(
-            |((condition, then_expr), else_expr)| ConditionalExpression {
-                condition: Box::new(condition),
-                then_expr: Box::new(then_expr),
-                else_expr: Box::new(else_expr),
-            },
-        )
+    macro_rules! op {
+        ($punct:expr) => {{
+            use Punctuator::*;
+            select! {
+                BalancedToken::Punctuator(p) if p == $punct => ()
+            }
+        }};
+    }
+
+    macro_rules! binary {
+        ($op:expr) => {
+            |left, _, right, _| {
+                use BinaryOperator::*;
+                Expression::Binary(BinaryExpression {
+                    operator: $op,
+                    left: Box::new(left),
+                    right: Box::new(right),
+                })
+            }
+        };
+    }
+
+    let unary = unary_expression(expression.clone());
+    let cast = cast_expression(unary.clone());
+    let postfix = postfix_expression(expression.clone());
+
+    // Suppose precedence is X, use 1000 - 10*X as the associativity level
+    choice((
+        unary.map(Expression::Unary),
+        cast.map(Expression::Cast),
+        postfix.map(Expression::Postfix),
+    ))
+    .pratt((
+        infix(left(1000 - 30), op!(Star), binary!(Multiply)),
+        infix(left(1000 - 30), op!(Slash), binary!(Divide)),
+        infix(left(1000 - 30), op!(Percent), binary!(Modulo)),
+        infix(left(1000 - 40), op!(Plus), binary!(Add)),
+        infix(left(1000 - 40), op!(Minus), binary!(Subtract)),
+        infix(left(1000 - 50), op!(LeftShift), binary!(LeftShift)),
+        infix(left(1000 - 50), op!(RightShift), binary!(RightShift)),
+        infix(left(1000 - 60), op!(Less), binary!(Less)),
+        infix(left(1000 - 60), op!(LessEqual), binary!(LessEqual)),
+        infix(left(1000 - 60), op!(Greater), binary!(Greater)),
+        infix(left(1000 - 60), op!(GreaterEqual), binary!(GreaterEqual)),
+        infix(left(1000 - 70), op!(Equal), binary!(Equal)),
+        infix(left(1000 - 70), op!(NotEqual), binary!(NotEqual)),
+        infix(left(1000 - 80), op!(Ampersand), binary!(BitwiseAnd)),
+        infix(left(1000 - 90), op!(Caret), binary!(BitwiseXor)),
+        infix(left(1000 - 100), op!(Pipe), binary!(BitwiseOr)),
+        infix(left(1000 - 110), op!(LogicalAnd), binary!(LogicalAnd)),
+        infix(left(1000 - 120), op!(LogicalOr), binary!(LogicalOr)),
+    ))
+    .labelled("binary expression")
+    .as_context()
+}
+
+pub fn conditional_expression<'a>(
+    binary_expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
+    expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
+) -> impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone {
+    recursive(|conditional_expression| {
+        let question = select! {
+            BalancedToken::Punctuator(Punctuator::Question) => ()
+        };
+        let colon = select! {
+            BalancedToken::Punctuator(Punctuator::Colon) => ()
+        };
+
+        choice((
+            binary_expression
+                .clone()
+                .then_ignore(question)
+                .then(expression)
+                .then_ignore(colon)
+                .then(conditional_expression)
+                .map(|((condition, then_expr), else_expr)| {
+                    Expression::Conditional(ConditionalExpression {
+                        condition: Box::new(condition),
+                        then_expr: Box::new(then_expr),
+                        else_expr: Box::new(else_expr),
+                    })
+                }),
+            binary_expression,
+        ))
+        .labelled("conditional expression")
+        .as_context()
+    })
+}
+
+pub fn assignment_expression<'a>(
+    conditional_expression: impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone + 'a,
+    unary_expression: impl Parser<'a, &'a [BalancedToken], UnaryExpression, Extra<'a>> + Clone + 'a,
+) -> impl Parser<'a, &'a [BalancedToken], Expression, Extra<'a>> + Clone {
+    recursive(|assignment_expression| {
+        let assigment_opeartor = select! {
+            BalancedToken::Punctuator(Punctuator::Assign) => AssignmentOperator::Assign,
+            BalancedToken::Punctuator(Punctuator::AddAssign) => AssignmentOperator::AddAssign,
+            BalancedToken::Punctuator(Punctuator::SubAssign) => AssignmentOperator::SubAssign,
+            BalancedToken::Punctuator(Punctuator::MulAssign) => AssignmentOperator::MulAssign,
+            BalancedToken::Punctuator(Punctuator::DivAssign) => AssignmentOperator::DivAssign,
+            BalancedToken::Punctuator(Punctuator::ModAssign) => AssignmentOperator::ModAssign,
+            BalancedToken::Punctuator(Punctuator::AndAssign) => AssignmentOperator::AndAssign,
+            BalancedToken::Punctuator(Punctuator::OrAssign) => AssignmentOperator::OrAssign,
+            BalancedToken::Punctuator(Punctuator::XorAssign) => AssignmentOperator::XorAssign,
+            BalancedToken::Punctuator(Punctuator::LeftShiftAssign) => AssignmentOperator::LeftShiftAssign,
+            BalancedToken::Punctuator(Punctuator::RightShiftAssign) => AssignmentOperator::RightShiftAssign,
+        };
+        choice((
+            unary_expression
+                .then(assigment_opeartor)
+                .then(assignment_expression)
+                .map(|((left, operator), right)| {
+                    Expression::Assignment(AssignmentExpression {
+                        operator,
+                        left: Box::new(Expression::Unary(left)),
+                        right: Box::new(right),
+                    })
+                }),
+            conditional_expression,
+        ))
+        .labelled("assignment expression")
+        .as_context()
+    })
 }
