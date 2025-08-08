acd6325d630a9a964c553bf76a35212daf4fcf5c
Jiayi Zhuang <wybxc@qq.com>
2025-08-09 00:30:56 +0800

    refactor: remove test_lexer function and streamline test_parser

--- tests/parse-test.rs
+++ tests/parse-test.rs
@@ -4,21 +4,6 @@ use rstest::rstest;
 use std::path::PathBuf;
 
 #[rstest]
-fn test_lexer(#[files("tests/test-cases/**/*.c")] path: PathBuf) {
-    let input = std::fs::read_to_string(&path).unwrap();
-    let lexer = balanced_token_sequence();
-
-    println!("Testing lexer on: {}", path.display());
-    let result = lexer.parse(&input);
-    if result.has_errors() {
-        for error in result.errors() {
-            println!("{error:?}");
-        }
-        panic!("Parsing failed with errors");
-    }
-}
-
-#[rstest]
 fn test_parser(#[files("tests/test-cases/**/*.c")] path: PathBuf) {
     use std::io::Write;
 
