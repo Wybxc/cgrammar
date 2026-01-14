0f57f0d97194030cc6b88564089cd91bc9949b8c
Jiayi Zhuang <wybxc@qq.com>
2026-01-14 15:55:29 +0800

    chore: update example shrink-test-case

--- examples/shrink_test_case.rs
+++ examples/shrink_test_case.rs
@@ -1,22 +1,35 @@
 //! Dump the AST of a C source file.
 //!
-//! Usage: `cargo run --example ast_dump --all-features -- path/to/source.c`
+//! Usage: `cargo run --example shrink_test_case --all-features -- path/to/source.c`
 
 use cgrammar::*;
 use chumsky::Parser;
 
-fn can_parse(file: &str, src: &str) -> bool {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ParseResult {
+    Success,
+    LexError,
+    ParseError,
+}
+
+fn can_parse(file: &str, src: &str) -> ParseResult {
     let lexer = balanced_token_sequence();
     let mut lexer_state = LexerState::new(Some(file));
     let tokens = lexer.parse_with_state(src, &mut lexer_state);
-    let Some(tokens) = tokens.output() else { return false };
+    let Some(tokens) = tokens.output() else {
+        return ParseResult::LexError;
+    };
 
     let parser = translation_unit();
     let mut init_state = State::new();
     init_state.ctx_mut().add_typedef_name("term".into());
     init_state.ctx_mut().add_typedef_name("thm".into());
     let ast = parser.parse_with_state(tokens.as_input(), &mut init_state);
-    !ast.has_errors()
+    if ast.has_errors() {
+        ParseResult::ParseError
+    } else {
+        ParseResult::Success
+    }
 }
 
 fn main() {
@@ -30,7 +43,7 @@ fn main() {
         let mid = (low + high) / 2;
         eprint!("{}...", mid);
         let truncated_src = src.lines().take(mid).collect::<Vec<_>>().join("\n");
-        if can_parse(&file, &truncated_src) {
+        if can_parse(&file, &truncated_src) == ParseResult::ParseError {
             low = mid + 1;
         } else {
             high = mid;
